aad1cf84f2a6c77e7b98001ce5afc43926851dfa
FuskYoy <zaraza_pikchers@outlook.com>
2022-11-13 14:50:40 +0700

    uyfre

--- Roblox/N.c
+++ Roblox/N.c
@@ -2,12 +2,16 @@
 
 int main(){
     int a=365;
-    int c;
+    int c=0;
     int f=100;
+    int m=0;
     for (int b=0; b<3;b++){ 
         c=a/f;
         a=a%f;
         f=f/10;
        printf("%d\n",c);
+       m=m+c;
     }
+    printf ("%d",m);
+
 }
